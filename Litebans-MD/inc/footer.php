d32f84d4f4723a67d0d84754fb4609a3eee61ca1
Unknown <johnbernheisel@me.com>
2019-07-23 22:03:14 -0400

    Hi

--- Litebans-MD/inc/footer.php
+++
@@ -1,5 +0,0 @@
-
-<script src="inc/js/popper.min.js"></script>
-<script src="<?php echo $this->autoversion('inc/js/jquery.min.js'); ?>"></script>
-<script src="<?php echo $this->autoversion('inc/js/bootstrap.min.js'); ?>"></script>
-<?php echo "</html>"; ?>
